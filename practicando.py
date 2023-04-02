a46aef3cd8412790468c56fac114aa3b73e95b55
LaraBerenice <berenice.ledesma12345@gmai.com>
2023-04-02 20:30:38 -0300

    /

--- practicando.py
+++ practicando.py
@@ -1,1 +1,22 @@
-print ("lara")
+
+
+
+print("tus datos son:"),print (type(3.2),type(3),type(True),type("lara"))
+
+berenice = "lara berenice"
+
+""" operadores de pertenencia ( in, not in)"""
+
+print (berenice  not in berenice )
+
+lara_berenice_ledesma = " persona sepsy y hermosa"
+
+print (lara_berenice_ledesma)
+
+lista = [3, "lara",True,8.4]
+
+print (lista[2])
+print (len(lista))
+print (type(lista))
+
+ 
